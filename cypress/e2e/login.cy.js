4e31d731bdd0529f5290ff1e3e3009630de7f561
daphne2016 <daphnemaria89@gmail.com>
2025-07-29 14:30:22 +0530

    scenario 2 updated

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,18 @@
+//scenario 1
+describe('cypress first test',()=>{
+it('Navigation',()=>{
+    cy.visit('/')
+    cy.url().should('contain','admlucid')
+    cy.title().should('eq','Home Page - Admlucid')
+
+})
+it('Login adm lucid',() =>{
+    cy.Login("daphnemaria89@gmail.com","hbjbdbb");
+
+})
+})
+   
+//scenario 2
 describe('cypress first test',()=>{
 it('Navigation',()=>{
     cy.visit('/')
